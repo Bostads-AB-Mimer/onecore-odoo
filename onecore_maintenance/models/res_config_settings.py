8e61fda90bab378d672e1f40d6c6be8d42d7785e
Johannes <erikjohanneskarlsson@gmail.com>
2024-03-20 15:31:56 +0100

    fix: broken settings page

--- onecore_maintenance/models/res_config_settings.py
+++ onecore_maintenance/models/res_config_settings.py
@@ -7,4 +7,4 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    module_maintenance_worksheet = fields.Boolean(string="Custom Maintenance Worksheets")
+    module_onecore_maintenance_worksheet = fields.Boolean(string="Custom Maintenance Worksheets")
