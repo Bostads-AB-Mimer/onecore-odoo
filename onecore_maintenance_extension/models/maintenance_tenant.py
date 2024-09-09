8fdcbdfe39f1cb71e160601807b38a026c2ee9ca
Linda Almqvist <linda.almqvist@prototyp.se>
2024-09-09 17:36:09 +0200

    split maintenance models into separate files

---
+++ onecore_maintenance_extension/models/maintenance_tenant.py
@@ -0,0 +1,26 @@
+from odoo import models, fields
+
+class OnecoreMaintenanceTenantOption(models.Model):
+    _name = 'maintenance.tenant.option'
+    _description = 'Tenant Option'
+
+    user_id = fields.Many2one('res.users', 'User', default=lambda self: self.env.user)
+    name = fields.Char('name', required=True)
+    contact_code = fields.Char('Contact Code', required=True)
+    contact_key = fields.Char('Contact Key', required=True)
+    national_registration_number = fields.Char('National Registration Number', required=True)
+    phone_number = fields.Char('Phone Number', required=True)
+    email_address = fields.Char('Email Address', required=True)
+    is_tenant = fields.Boolean('Is Tenant', default=True)
+
+class OnecoreMaintenanceTenant(models.Model):
+    _name = 'maintenance.tenant'
+    _description = 'Tenant'
+
+    name = fields.Char('name', required=True)
+    contact_code = fields.Char('Contact Code', required=True)
+    contact_key = fields.Char('Contact Key', required=True)
+    national_registration_number = fields.Char('National Registration Number', required=True)
+    phone_number = fields.Char('Phone Number', required=True)
+    email_address = fields.Char('Email Address', required=True)
+    is_tenant = fields.Boolean('Is Tenant', default=True)
