03e909fc5cf83ba05e108d63defce9008b52c8e1
Johannes <erikjohanneskarlsson@gmail.com>
2024-09-12 12:21:19 +0200

    hook logs variables from kubernetes

--- onecore_auth/hooks/__init__.py
+++ onecore_auth/hooks/__init__.py
@@ -5,8 +5,22 @@ def _post_init_hook(env):
     _insert_env_variables(env)
 
 def _insert_env_variables(env):
-    config = dotenv_values(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
-    if not config:
-        config = dotenv_values(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env.template'))
-    for key, value in config.items():
-        env['ir.config_parameter'].set_param(key, value)
+    # Load variables from .env.template
+    env_template_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env.template')
+    env_template_config = dotenv_values(template_path)
+
+    # Check if all variables from .env.template are present in odoo-env secret
+    all_in_secret = all(var in os.environ for var in env_template_config)
+
+    if all_in_secret:
+        for key in env_template_config:
+            env['ir.config_parameter'].set_param(key, os.environ[key])
+    else:
+        env_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+        env_config = dotenv_values(env_path)
+
+        for key, value in template_config.items():
+            if key in env_config:
+                env['ir.config_parameter'].set_param(key, env_config[key])
+            else:
+                env['ir.config_parameter'].set_param(key, value)
