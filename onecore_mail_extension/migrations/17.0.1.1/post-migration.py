c9f515c6577fed0dbb7c93972157e4a7c7f5d7ba
Johannes <erikjohanneskarlsson@gmail.com>
2024-10-16 15:06:52 +0200

    MIM-166: adds notifications from Mimer.nu on cards

---
+++ onecore_mail_extension/migrations/17.0.1.1/post-migration.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+def migrate(cr, version):
+    """
+    This migration sets the default value of internal mail message subtypes to true.
+    This is done to ensure that followers gets notified on notes and activities by default.
+    """
+    cr.execute('UPDATE mail_message_subtype SET "default" = true WHERE "internal" = true')
