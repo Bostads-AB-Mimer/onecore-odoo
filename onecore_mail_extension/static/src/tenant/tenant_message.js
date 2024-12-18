bd10c970002ec263728e2c21fcfa59c8dbc9464e
Henrik Persson <henrik.persson@iteam.com>
2024-12-18 11:40:11 +0100

    Make messages collapsible

---
+++ onecore_mail_extension/static/src/tenant/tenant_message.js
@@ -0,0 +1,28 @@
+/** @odoo-module **/
+
+import { patch } from '@web/core/utils/patch';
+import { Message } from '@mail/core/common/message';
+import { browser } from "@web/core/browser/browser";
+import { useRef, useExternalListener, onMounted } from '@odoo/owl'
+
+patch(Message.prototype, {
+    setup() {
+        super.setup();
+        this.messageRef = useRef('messageRef');
+        this.isCollapsible = true;
+        this.isCollapsed = true;
+
+        useExternalListener(browser, "resize", this.setIsCollapsible);
+
+        onMounted(async () => {
+            this.setIsCollapsible();
+            this.isCollapsed = this.isCollapsible;
+        });
+    },
+    setIsCollapsible() {
+        this.isCollapsible = this.messageRef.el.clientHeight > 100;
+    },
+    toggleIsCollapsed() {
+        this.isCollapsed = !this.isCollapsed;
+    },
+});
