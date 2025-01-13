a60fe4c8ffff6e59ac62607580aeeb7f2017bb5c
Henrik Persson <henrik.persson@iteam.com>
2025-01-13 16:33:47 +0100

    Format all python files

--- onecore_mail_extension/__manifest__.py
+++ onecore_mail_extension/__manifest__.py
@@ -1,24 +1,22 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'ONECore Mail Extension',
-    'version': '1.1',
-    'sequence': 100,
-    'category': 'Productivity/Discuss',
-    'description': 'Extends the mail module with ONECore features.',
-    'depends': ['base', 'mail'],
-    'summary': 'Extends the mail module with ONECore features.',
-    'assets': {
-        'web._assets_primary_variables': [
-            'mail/static/src/**/primary_variables.scss',
+    "name": "ONECore Mail Extension",
+    "version": "1.1",
+    "sequence": 100,
+    "category": "Productivity/Discuss",
+    "description": "Extends the mail module with ONECore features.",
+    "depends": ["base", "mail"],
+    "summary": "Extends the mail module with ONECore features.",
+    "assets": {
+        "web._assets_primary_variables": [
+            "mail/static/src/**/primary_variables.scss",
         ],
-        'web.assets_backend': [
-            'onecore_mail_extension/static/src/tenant/**/*',
+        "web.assets_backend": [
+            "onecore_mail_extension/static/src/tenant/**/*",
         ],
-
-        
     },
-    'website': 'https://www.odoo.com/app/discuss',
-    'auto_install': True,
-    'license': 'LGPL-3',
+    "website": "https://www.odoo.com/app/discuss",
+    "auto_install": True,
+    "license": "LGPL-3",
 }
--- onecore_ui/__manifest__.py
+++ onecore_ui/__manifest__.py
@@ -1,17 +1,17 @@
 {
-    'name': 'ONECore UI',
-    'version': '1.0',
-    'sequence': 100,
-    'category': 'ONECore',
-    'description': 'Provides ONECore UI',
-    'depends': [],
-    'summary': 'Provides ONECore UI',
-    'assets': {
-        'web.assets_backend': [
-            'onecore_ui/static/src/js/*.js',
-            'onecore_ui/static/src/views/*.xml',
+    "name": "ONECore UI",
+    "version": "1.0",
+    "sequence": 100,
+    "category": "ONECore",
+    "description": "Provides ONECore UI",
+    "depends": [],
+    "summary": "Provides ONECore UI",
+    "assets": {
+        "web.assets_backend": [
+            "onecore_ui/static/src/js/*.js",
+            "onecore_ui/static/src/views/*.xml",
         ],
     },
-    'auto_install': True,
-    'license': 'LGPL-3',
+    "auto_install": True,
+    "license": "LGPL-3",
 }
