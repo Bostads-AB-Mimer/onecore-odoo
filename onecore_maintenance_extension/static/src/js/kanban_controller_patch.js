9997bf35ed8d17e7dfb8c071fda52a0bdf71138b
Henrik Persson <henrik.persson@iteam.com>
2025-01-22 16:33:54 +0100

    More formatting

--- onecore_maintenance_extension/static/src/js/kanban_controller_patch.js
+++ onecore_maintenance_extension/static/src/js/kanban_controller_patch.js
@@ -1,43 +1,43 @@
 /** @odoo-module **/
 
-import { KanbanController } from '@web/views/kanban/kanban_controller'
-import { onWillStart } from '@odoo/owl'
-import { useService } from '@web/core/utils/hooks'
-import { patch } from '@web/core/utils/patch'
+import { KanbanController } from "@web/views/kanban/kanban_controller";
+import { onWillStart } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
+import { patch } from "@web/core/utils/patch";
 
-// This patch is used to prevent external contractors from creating maintenance requests 
+// This patch is used to prevent external contractors from creating maintenance requests
 // using the button in the kanban view.
 
 patch(KanbanController.prototype, {
   setup(env, services) {
-    super.setup(env, services)
-    this.rpc = useService('rpc')
-    this.isExternalContractor
+    super.setup(env, services);
+    this.rpc = useService("rpc");
+    this.isExternalContractor;
 
     onWillStart(async () => {
-      const isExternalContractor = await this.rpc('/web/dataset/call_kw', {
-        model: 'maintenance.request',
-        method: 'is_user_external_contractor',
+      const isExternalContractor = await this.rpc("/web/dataset/call_kw", {
+        model: "maintenance.request",
+        method: "is_user_external_contractor",
         args: [],
         kwargs: {},
-      })
-      this.isExternalContractor = isExternalContractor
-    })
+      });
+      this.isExternalContractor = isExternalContractor;
+    });
   },
   get canCreate() {
-    const { create, createGroup } = this.props.archInfo.activeActions
-    const list = this.model.root
-    if (this.isExternalContractor) return false
+    const { create, createGroup } = this.props.archInfo.activeActions;
+    const list = this.model.root;
+    if (this.isExternalContractor) return false;
 
     if (!create) {
-      return false
+      return false;
     }
     if (list.isGrouped) {
-      if (list.groupByField.type !== 'many2one') {
-        return true
+      if (list.groupByField.type !== "many2one") {
+        return true;
       }
-      return list.groups.length || !createGroup
+      return list.groups.length || !createGroup;
     }
-    return true
+    return true;
   },
-})
+});
