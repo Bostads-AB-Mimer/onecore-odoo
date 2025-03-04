ad6e9a4d298de5c83de9f9a5314fbad03d952b84
Linda Almqvist <34321514+lalmqvist@users.noreply.github.com>
2025-03-04 13:31:54 +0100

    MIM-398: handle added tenant as ny kundinfo (#126)
    * handle added tenant as ny kundinfo
    
    * update use of empty tenant
    
    * rename added_tenant to recently_added_tenant

--- onecore_maintenance_extension/models/maintenance.py
+++ onecore_maintenance_extension/models/maintenance.py
@@ -258,14 +258,30 @@ class OneCoreMaintenanceRequest(models.Model):
         store=False,
     )
 
-    new_tenant = fields.Boolean(
-        string="New Tenant", store=False, compute="_compute_new_tenant"
+    empty_tenant = fields.Boolean(
+        string="No tenant", store=False, compute="_compute_empty_tenant"
     )
 
-    @api.model
-    def _compute_new_tenant(self):
+    recently_added_tenant = fields.Boolean(
+        string="Recently added tenant", store=True, default=False
+    )
+
+    @api.depends("recently_added_tenant")
+    def _compute_empty_tenant(self):
         for record in self:
-            record.new_tenant = False
+            if record.lease_name and record.create_date or not record.create_date:
+                record.empty_tenant = False
+            else:
+                record.empty_tenant = True
+
+        if self.recently_added_tenant and self.tenant_id:
+            # Check if the tenant was created more than two weeks ago
+            if (datetime.datetime.now() - self.tenant_id.create_date).days > 14:
+                for record in self:
+                    record.recently_added_tenant = False
+        else:
+            for record in self:
+                record.recently_added_tenant = False
 
         if self.rental_property_id and not self.lease_id:  # Empty tenant / lease
             data = self.fetch_property_data(
@@ -310,7 +326,7 @@ class OneCoreMaintenanceRequest(models.Model):
                                 name = self._get_tenant_name(tenant)
                                 phone_number = self._get_main_phone_number(tenant)
 
-                                new_tenant_record = self.env[
+                                recently_added_tenant_record = self.env[
                                     "maintenance.tenant"
                                 ].create(
                                     {
@@ -327,8 +343,9 @@ class OneCoreMaintenanceRequest(models.Model):
                                 )
 
                                 for record in self:
-                                    record.tenant_id = new_tenant_record.id
-                                    record.new_tenant = True
+                                    record.tenant_id = recently_added_tenant_record.id
+                                    record.recently_added_tenant = True
+                                    record.empty_tenant = False
 
     def _get_tenant_name(self, tenant):
         """
