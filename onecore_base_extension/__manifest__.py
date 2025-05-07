f31417c843a4e0024ebecc48c51d82bdfbcb5150
Simon Kropp <simon.kropp@prototyp.se>
2025-05-07 15:22:27 +0200

    create onecore_base_extension and hide api keys from preferences for non-admins

---
+++ onecore_base_extension/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "OneCore Base Extension",
+    "category": "ONECore",
+    "summary": "Extends the Odoo base module with ONECore specific customizations.",
+    "sequence": 100,
+    "category": "Productivity/Discuss",
+    "version": "1.0",
+    "depends": ["base"],
+    "data": [
+        "views/res_users_view.xml",
+    ],
+    "installable": True,
+}
