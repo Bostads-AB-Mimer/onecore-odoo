b55a46e1c13e5d18f173d61fe2ad7181411b96c9
Johannes Karlsson <47555484+johanneskarlsson@users.noreply.github.com>
2025-08-26 14:06:21 +0200

    adds keycloak hook (#183)

---
+++ onecore_auth/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "ONECore Authentication",
+    "version": "1.0",
+    "sequence": 50,
+    "category": "Authentication",
+    "description": "Keycloak authentication integration for ONECore.",
+    "depends": ["base", "web"],
+    "summary": "Keycloak authentication setup for ONECore.",
+    "data": [],
+    "post_init_hook": "_post_init_hook",
+    "auto_install": True,
+    "license": "LGPL-3",
+}
