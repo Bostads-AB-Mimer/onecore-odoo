f6ac92bf48b6c3f7677642f49b3ac75357b10cfd
lindblomdavid <65215239+lindblomdavid@users.noreply.github.com>
2025-12-05 13:55:44 +0100

    MIM-1058: Skapa component genom bild och berget AI

--- onecore_maintenance_extension/models/maintenance.py
+++ onecore_maintenance_extension/models/maintenance.py
@@ -659,3 +659,16 @@ class OneCoreMaintenanceRequest(
             "url": url,
             "target": "self",
         }
+
+    def open_component_wizard(self):
+        self.ensure_one()
+        return {
+            "name": "Uppdatera/lägg till Komponent",
+            "type": "ir.actions.act_window",
+            "res_model": "maintenance.component.wizard",
+            "view_mode": "form",
+            "view_type": "form",
+            "views": [(False, "form")],
+            "target": "new",
+            "context": {"default_maintenance_request_id": self.id},
+        }
