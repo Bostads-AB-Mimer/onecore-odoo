36621659a0b1ffd96e3a76a7e5f9c5fbbcaf9e00
crewiz <s3salas@gmail.com>
2026-03-20 08:46:36 +0100

    Extension upgrade done - but cant be tested until maintenance is done

--- onecore_web_extension/static/src/js/kanban_header_patch.js
+++ onecore_web_extension/static/src/js/kanban_header_patch.js
@@ -1,45 +1,21 @@
 /* @odoo-module */
 
-import { _t } from "@web/core/l10n/translation";
-import { registry } from "@web/core/registry";
 import { patch } from "@web/core/utils/patch";
 import { KanbanHeader } from "@web/views/kanban/kanban_header";
 
 patch(KanbanHeader.prototype, {
-  setup() {
-    super.setup();
-  },
-
   /**
-   * @override
+   * Override configMenuProps to remove the edit and delete group
+   * options from the dropdown menu, for ALL USERS.
+   *
+   * This prevents users from editing or deleting kanban groups.
    */
-
-  get configItems() {
-    /**
-     *
-     * Modifying the kanban header config items to remove the edit and delete group
-     * options from the dropdown menu, for ALL USERS.
-     *
-     * This is done to prevent users from editing or deleting the kanban group
-     */
-
-    const args = { permissions: this.permissions, props: this.props };
-    return registry
-      .category("kanban_header_config_items")
-      .getEntries()
-      .filter(
-        ([_, desc]) =>
-          desc.method !== "editGroup" && desc.method !== "deleteGroup"
-      )
-      .map(([key, desc]) => ({
-        key,
-        method: desc.method,
-        label: desc.label,
-        isVisible:
-          typeof desc.isVisible === "function"
-            ? desc.isVisible(args)
-            : desc.isVisible,
-        class: typeof desc.class === "function" ? desc.class(args) : desc.class,
-      }));
+  get configMenuProps() {
+    const result = super.configMenuProps;
+    result.configItems = result.configItems.filter(
+      ([_, desc]) =>
+        desc.method !== "editGroup" && desc.method !== "deleteGroup",
+    );
+    return result;
   },
 });
