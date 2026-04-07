f08512a9c4b6915f7f038c2e9020dd103f978f5e
lindblomdavid <65215239+lindblomdavid@users.noreply.github.com>
2026-04-07 10:00:16 +0200

    removed odoo-bot messages and channel

--- onecore_base_extension/__manifest__.py
+++ onecore_base_extension/__manifest__.py
@@ -6,9 +6,14 @@
     "summary": "Extends the Odoo base module with ONECore specific customizations.",
     "sequence": 100,
     "version": "19.0.1.0.0",
-    "depends": ["base"],
+    "depends": ["base", "mail_bot", "mail"],
     "data": [
         "views/res_users_view.xml",
     ],
+    "assets": {
+        "web.assets_backend": [
+            "onecore_base_extension/static/src/xml/messaging_menu.xml",
+        ],
+    },
     "auto_install": True,
 }
