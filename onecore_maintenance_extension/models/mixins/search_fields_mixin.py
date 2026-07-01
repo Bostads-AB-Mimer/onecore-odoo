b7b50d2c96266efccc47ed5e0fce1f802c63485c
Simon Kropp <simon.kropp@prototyp.se>
2026-07-01 16:41:29 +0200

    fix(mim-1841): give tenant_lookup a distinct label to avoid duplicate-label warning

--- onecore_maintenance_extension/models/mixins/search_fields_mixin.py
+++ onecore_maintenance_extension/models/mixins/search_fields_mixin.py
@@ -20,7 +20,7 @@ class SearchFieldsMixin(models.AbstractModel):
 
     # Paste-to-populate lookups (MIM-1841): transient inputs, never stored.
     rental_object_lookup = fields.Char("Objektnummer", store=False)
-    tenant_lookup = fields.Char("Kundnummer", store=False)
+    tenant_lookup = fields.Char("Slå upp hyresgäst", store=False)
 
     # Search option fields (populated by handlers, transient - not stored)
     property_option_id = fields.Many2one(
